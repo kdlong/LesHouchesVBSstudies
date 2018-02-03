712d033806ccf1772c7739f564d9bf228c4e54c3
Kenneth Long <kdlong@hep.wisc.edu>
2018-02-03 17:50:38 +0100

    Add MadGraph and VBFNLO+Herwig Yoda files

--- Rivet/make_plots_WZVBS.sh
+++ Rivet/make_plots_WZVBS.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # add 
-MGCENTRAL="'Title={MadGraph_aMC@NLO}:ErrorBands=1:ErrorBandOpacity=0.1:LineWidth=0.02'"
-MGFIXEDMZ="'Scale=0.33:Title={MadGraph_aMC@NLO fixed $\mu=m_Z$}:ErrorBands=1:ErrorBandColor=orange:ErrorBandOpacity=0.1:LineWidth=0.02'"
+#MGCENTRAL="'Title={MadGraph_aMC@NLO}:ErrorBands=1:ErrorBandOpacity=0.1:LineWidth=0.02'"
+#MGFIXEDMZ="'Scale=0.33:Title={MadGraph_aMC@NLO fixed $\mu=m_Z$}:ErrorBands=1:ErrorBandColor=orange:ErrorBandOpacity=0.1:LineWidth=0.02'"
 # use the ones below instead to set also the line color 
 #MGCENTRAL="'Title={MadGraph_aMC@NLO}:ErrorBands=1:ErrorBandColor=magenta:LineColor=magenta:ErrorBandOpacity=0.1:LineWidth=0.02'"
 #MGFIXEDMZ="'Scale=1.0:Title={MadGraph_aMC@NLO fixed $\mu=m_Z$}:ErrorBands=1:ErrorBandColor=teal:LineColor=teal:ErrorBandOpacity=0.1:LineWidth=0.02'"
