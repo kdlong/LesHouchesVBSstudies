65b8edb4010e96f38e18b01237ff8120ff5d0da5
Vitaliano Ciulli <vitaliano.ciulli@cern.ch>
2018-02-07 06:16:37 +0100

    add plot files

---
+++ proceedings/figs/copy_plots.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+cp ../../Rivet/plotting/rivet-plots-MG-VBFNLO-FIXEDMW/WZVBS_LesHouchesStudy/WmZ_OF_zepj3.pdf  VBFNLO_WmZ_OF_zepj3.pdf
+cp ../../Rivet/plotting/rivet-plots-MG-VBFNLO-FIXEDMW/WZVBS_LesHouchesStudy/WmZ_OF_zep3l.pdf  VBFNLO_WmZ_OF_zep3l.pdf
+cp ../../Rivet/plotting/rivet-plots-MG-VBFNLO-FIXEDMW/WZVBS_LesHouchesStudy/WmZ_OF_mjj.pdf    VBFNLO_WmZ_OF_mjj.pdf
+cp ../../Rivet/plotting/rivet-plots-MG-VBFNLO-FIXEDMW/WZVBS_LesHouchesStudy/WmZ_OF_dRjj.pdf   VBFNLO_WmZ_OF_dRjj.pdf
+cp ../../Rivet/plotting/rivet-plots-MG-VBFNLO-FIXEDMW/WZVBS_LesHouchesStudy/WmZ_OF_ZPt.pdf    VBFNLO_WmZ_OF_ZPt.pdf
+cp ../../Rivet/plotting/rivet-plots-MG-VBFNLO-FIXEDMW/WZVBS_LesHouchesStudy/WmZ_OF_dEtajj.pdf VBFNLO_WmZ_OF_dEtajj.pdf
+cp ../../Rivet/plotting/rivet-plots-MG-VBFNLO-FIXEDMW/WZVBS_LesHouchesStudy/WmZ_OF_nJets.pdf  VBFNLO_WmZ_OF_nJets.pdf
+
+cp ../../Rivet/plotting/rivet-plots-MG-Wm/WZVBS_LesHouchesStudy/WmZ_OF_zepj3.pdf  MG_WmZ_OF_zepj3.pdf
+cp ../../Rivet/plotting/rivet-plots-MG-Wm/WZVBS_LesHouchesStudy/WmZ_OF_zep3l.pdf  MG_WmZ_OF_zep3l.pdf
+cp ../../Rivet/plotting/rivet-plots-MG-Wm/WZVBS_LesHouchesStudy/WmZ_OF_mjj.pdf    MG_WmZ_OF_mjj.pdf
+cp ../../Rivet/plotting/rivet-plots-MG-Wm/WZVBS_LesHouchesStudy/WmZ_OF_dRjj.pdf   MG_WmZ_OF_dRjj.pdf
+cp ../../Rivet/plotting/rivet-plots-MG-Wm/WZVBS_LesHouchesStudy/WmZ_OF_ZPt.pdf    MG_WmZ_OF_ZPt.pdf
+cp ../../Rivet/plotting/rivet-plots-MG-Wm/WZVBS_LesHouchesStudy/WmZ_OF_dEtajj.pdf MG_WmZ_OF_dEtajj.pdf
+cp ../../Rivet/plotting/rivet-plots-MG-Wm/WZVBS_LesHouchesStudy/WmZ_OF_nJets.pdf  MG_WmZ_OF_nJets.pdf
+
+cp ../../Rivet/plotting/rivet-plots-MG-all/WZVBS_LesHouchesStudy/WmZ_OF_zepj3.pdf  MG_WZ_OF_zepj3.pdf
+cp ../../Rivet/plotting/rivet-plots-MG-all/WZVBS_LesHouchesStudy/WmZ_OF_zep3l.pdf  MG_WZ_OF_zep3l.pdf
+cp ../../Rivet/plotting/rivet-plots-MG-all/WZVBS_LesHouchesStudy/WmZ_OF_mjj.pdf    MG_WZ_OF_mjj.pdf
+cp ../../Rivet/plotting/rivet-plots-MG-all/WZVBS_LesHouchesStudy/WmZ_OF_dRjj.pdf   MG_WZ_OF_dRjj.pdf
+cp ../../Rivet/plotting/rivet-plots-MG-all/WZVBS_LesHouchesStudy/WmZ_OF_ZPt.pdf    MG_WZ_OF_ZPt.pdf
+cp ../../Rivet/plotting/rivet-plots-MG-all/WZVBS_LesHouchesStudy/WmZ_OF_dEtajj.pdf MG_WZ_OF_dEtajj.pdf
+cp ../../Rivet/plotting/rivet-plots-MG-all/WZVBS_LesHouchesStudy/WmZ_OF_nJets.pdf  MG_WZ_OF_nJets.pdf
+
