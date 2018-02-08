0da2e3b7a0b7334197eb9f1efd8ac2f5336abcbd
Vitaliano Ciulli <vitaliano.ciulli@cern.ch>
2018-02-08 07:43:10 +0100

    add sherpa results

--- proceedings/figs/copy_plots.sh
+++ proceedings/figs/copy_plots.sh
@@ -23,3 +23,19 @@ cp ../../Rivet/plotting/rivet-plots-MG-all/WZVBS_LesHouchesStudy/ZPt.pdf    MG_Z
 cp ../../Rivet/plotting/rivet-plots-MG-all/WZVBS_LesHouchesStudy/dEtajj.pdf MG_dEtajj.pdf
 cp ../../Rivet/plotting/rivet-plots-MG-all/WZVBS_LesHouchesStudy/nJets.pdf  MG_nJets.pdf
 
+
+cp ../../Rivet/plotting/rivet-plots-OF/WZVBS_LesHouchesStudy/WmZ_OF_zepj3.pdf  WmZ_OF_zepj3.pdf
+cp ../../Rivet/plotting/rivet-plots-OF/WZVBS_LesHouchesStudy/WmZ_OF_zep3l.pdf  WmZ_OF_zep3l.pdf
+cp ../../Rivet/plotting/rivet-plots-OF/WZVBS_LesHouchesStudy/WmZ_OF_mjj.pdf    WmZ_OF_mjj.pdf
+cp ../../Rivet/plotting/rivet-plots-OF/WZVBS_LesHouchesStudy/WmZ_OF_dRjj.pdf   WmZ_OF_dRjj.pdf
+cp ../../Rivet/plotting/rivet-plots-OF/WZVBS_LesHouchesStudy/WmZ_OF_ZPt.pdf    WmZ_OF_ZPt.pdf
+cp ../../Rivet/plotting/rivet-plots-OF/WZVBS_LesHouchesStudy/WmZ_OF_dEtajj.pdf WmZ_OF_dEtajj.pdf
+cp ../../Rivet/plotting/rivet-plots-OF/WZVBS_LesHouchesStudy/WmZ_OF_nJets.pdf  WmZ_OF_nJets.pdf
+cp ../../Rivet/plotting/rivet-plots-OF/WZVBS_LesHouchesStudy/WpZ_OF_zepj3.pdf  WpZ_OF_zepj3.pdf
+cp ../../Rivet/plotting/rivet-plots-OF/WZVBS_LesHouchesStudy/WpZ_OF_zep3l.pdf  WpZ_OF_zep3l.pdf
+cp ../../Rivet/plotting/rivet-plots-OF/WZVBS_LesHouchesStudy/WpZ_OF_mjj.pdf    WpZ_OF_mjj.pdf
+cp ../../Rivet/plotting/rivet-plots-OF/WZVBS_LesHouchesStudy/WpZ_OF_dRjj.pdf   WpZ_OF_dRjj.pdf
+cp ../../Rivet/plotting/rivet-plots-OF/WZVBS_LesHouchesStudy/WpZ_OF_ZPt.pdf    WpZ_OF_ZPt.pdf
+cp ../../Rivet/plotting/rivet-plots-OF/WZVBS_LesHouchesStudy/WpZ_OF_dEtajj.pdf WpZ_OF_dEtajj.pdf
+cp ../../Rivet/plotting/rivet-plots-OF/WZVBS_LesHouchesStudy/WpZ_OF_nJets.pdf  WpZ_OF_nJets.pdf
+
