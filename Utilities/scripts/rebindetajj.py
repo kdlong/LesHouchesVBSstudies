59b76b0a68485859d0dc964df6151ad92e87a722
Kenneth Long <kdlong@hep.wisc.edu>
2018-02-20 15:28:33 +0100

    Rebin etajj for Sherpa as well

--- Utilities/scripts/rebindetajj.py
+++ Utilities/scripts/rebindetajj.py
@@ -1,21 +1,31 @@
 # coding: utf-8
 import ROOT
 import math
-recola_file = ROOT.TFile("histogram_rapidity_separation_j1j2_born.root")
-recola_hist = recola_file.Get("canvas").GetListOfPrimitives().FindObject("histogram_rapidity_separation_j1j2_born")
-mg_file = ROOT.TFile("/afs/cern.ch/user/k/kelong/work/DibosonMCAnalysis/CMSSW_8_0_30/src/GenNtuplizer/Utilities/PlotingScripts/MGHists/MGPartonPlots-nobquarks-mathieusSetup_wmonly.root")
-mg_hist = mg_file.Get("hdeta")
-    
-new_recola_hist = mg_hist.Clone()
-for i in range(mg_hist.GetNbinsX()+1): new_recola_hist.SetBinContent(i,0)
 
-bins = [2.25 + x*0.25 for x in range(0,26)]
-for x in bins:
-    new_content = recola_hist.GetBinContent(recola_hist.FindBin(x)) + recola_hist.GetBinContent(recola_hist.FindBin(x*-1-0.001))
-    new_error = math.sqrt(recola_hist.GetBinError(recola_hist.FindBin(x))**2+recola_hist.GetBinError(recola_hist.FindBin(x*-1-0.001))**2)
-    new_recola_hist.SetBinContent(mg_hist.FindBin(x), new_content)
-    new_recola_hist.SetBinError(mg_hist.FindBin(x), new_error)
+isSherpa=True
+
+if isSherpa:
+    input_file = ROOT.TFile("../../Rivet/data/Sherpa_FO.root")
+    input_hist = input_file.Get("WZVBS_LesHouchesStudy/dEtajj")
+    output_name = "Sherpa_FO_etajj.root"
+else:
+    input_file = ROOT.TFile("histogram_rapidity_separation_j1j2_born.root")
+    input_hist = input_file.Get("canvas").GetListOfPrimitives().FindObject("histogram_rapidity_separation_j1j2_born")
+    output_name = "recola_etajj.root"
+
+ref_file = ROOT.TFile("/afs/cern.ch/user/k/kelong/work/DibosonMCAnalysis/CMSSW_8_0_30/src/GenNtuplizer/Utilities/PlotingScripts/MGHists/MGPartonPlots-nobquarks-mathieusSetup_wmonly.root")
+ref_hist = ref_file.Get("hdeta")
     
-outfile = ROOT.TFile("recola_etajj.root", "recreate")
-new_recola_hist.Write()
+new_input_hist = ref_hist.Clone()
+for i in range(ref_hist.GetNbinsX()+1): new_input_hist.SetBinContent(i,0)
+
+bins = [2.25 + x*0.25 for x in range(0,23 if isSherpa else 26)]
+for x in bins:
+    new_content = input_hist.GetBinContent(input_hist.FindBin(x)) + input_hist.GetBinContent(input_hist.FindBin(x*-1-0.001))
+    new_error = math.sqrt(input_hist.GetBinError(input_hist.FindBin(x))**2+input_hist.GetBinError(input_hist.FindBin(x*-1-0.001))**2)
+    new_input_hist.SetBinContent(ref_hist.FindBin(x), new_content)
+    new_input_hist.SetBinError(ref_hist.FindBin(x), new_error)
+    
+outfile = ROOT.TFile(output_name, "recreate")
+new_input_hist.Write()
 outfile.Close()
